4615bd9bbbb52811fbce914629a849d631a7c2d2
ElBomberitoFeliz <jeisonortiz0016@gmail.com>
2020-09-01 12:35:51 -0500

    config

--- sql/prueba.php
+++ sql/prueba.php
@@ -9,7 +9,8 @@
     ADDR: Cl. 6 Sur ## 24-127, Madrid, Cundinamarca
     ID: 001
     MAPA: https://www.google.com/maps/place/Conjunto+Residencial+Boreal/@4.7329996,-74.2841813,20.29z/data=!4m5!3m4!1s0x8e3f79da7a9d91e3:0xa1c55d6a64ecb0e5!8m2!3d4.7327344!4d-74.2844667
-    RIESGO: MODERADO";
+    RIESGO: MODERADO
+    GRUPO: Pruebas";
     $headers = "From:" . $from;
     mail($to,$subject,$message, $headers);
     echo "The email message was sent.";
